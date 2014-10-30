b4c1231380369606ea82abb5030848bfdc3d9156
viracsreng <srengvirac@hotmail.com>
2014-10-30 15:13:00 +0100

    initial commit

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
-var azure = require('azure');
-// var formidable = require('formidable');
-// var fs = require('fs');
-// var jf = require('jsonfile');
-// var url = require('url');
-
 var app = express();
 
 // view engine setup
