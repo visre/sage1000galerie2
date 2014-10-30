8025760b75ee14f946f3b21206cb837318245581
viracsreng <srengvirac@hotmail.com>
2014-10-30 15:10:26 +0100

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var azure = require('azure');
+var formidable = require('formidable');
+var fs = require('fs');
+var jf = require('jsonfile');
+var url = require('url');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
